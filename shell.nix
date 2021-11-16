f71263fb2c00c88d5ceb2f8701752b41d8df317a
cateru <cateru.dev@zohomail.com>
2021-11-16 17:28:40 +0100

    SHELL setup

--- shell.nix
+++ shell.nix
@@ -3,11 +3,12 @@ with (import <nixpkgs> {});
 mkShell {
   buildInputs = [
     nodejs-17_x
+    nodePackages.pnpm
   ];
   shellHook = ''
-      mkdir -p .nix-node
-      export NODE_PATH=$PWD/.nix-node
-      export NPM_CONFIG_PREFIX=$PWD/.nix-node
-      export PATH=$NODE_PATH/bin:$PATH
+      # mkdir -p .nix-node
+      # export NODE_PATH=$PWD/.nix-node
+      # export NPM_CONFIG_PREFIX=$PWD/.nix-node
+      # export PATH=$NODE_PATH/bin:$PATH
   '';
 }
