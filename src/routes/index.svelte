d0bca3185a66fe4f92314726ade711059c619e34
cateru <cateru.dev@zohomail.com>
2021-11-23 14:38:57 +0100

    + Strict Eslint + Enforce Functional style

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-	export let name: string;
-  
-    
+    export let name: string;
 </script>
-<h1 class="text-3xl text-left  mb-[30px]" >Welcome to SvelteKit {name}</h1>
-     <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-
- 
+<h1 class="text-3xl  text-center">
+    Welcome to SvelteKit {name}
+</h1>
+<p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a>
+    to read the documentation
+</p>
--- src/routes/index.test.ts
+++ src/routes/index.test.ts
@@ -1,9 +1,9 @@
 // import { render } from '@testing-library/svelte';
-import { Page } from "playwright"
 test('is jest working', () => {
-	expect(5+5).toBe(10);
+	expect(5 + 5).toBe(10);
 });
-test('is playwright working', async() => {
-  await page.goto("https://www.google.com/")
-  await browser.close()
+test('is playwright working', async () => {
+	await page.goto('https://www.google.com/');
+	await browser.close();
 });
+export {};
